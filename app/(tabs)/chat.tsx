4b0626c498d34db84f17daea87e35fb7b965176f
dihlogg <dinhlongg171204@gmail.com>
2025-04-21 21:00:20 +0700

    #refactor-history-order-details

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,13 +1,11 @@
-import {
-  View,
-  StyleSheet,
-  Platform,
-} from "react-native";
-import React from "react";
+import { View, StyleSheet, Platform } from "react-native";
+import React, { useEffect, useState } from "react";
 import { useHeaderHeight } from "@react-navigation/elements";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Stack } from "expo-router";
 import ChatWootView from "@/components/ChatWootView";
+import { RegisterRequest } from "@/types/register-request";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const ChatScreen = () => {
   const headerHeight = useHeaderHeight();
@@ -16,8 +14,10 @@ const ChatScreen = () => {
     <GestureHandlerRootView style={styles.root}>
       <Stack.Screen options={{ headerShown: true, headerTransparent: true }} />
       <View style={[styles.container, { marginTop: headerHeight }]}>
-        {Platform.OS !== "web" && <ChatWootView />}
-      </View>
+          {Platform.OS !== "web" && (
+            <ChatWootView />
+          )}
+      </View>      
     </GestureHandlerRootView>
   );
 };
